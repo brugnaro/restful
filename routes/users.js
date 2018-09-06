0e9043b94bf1156b6eb207408433f005fa18d110
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-09-05 22:42:38 -0300

    Finish project.

--- routes/users.js
+++ routes/users.js
@@ -6,23 +6,66 @@ let db = new NeDB({
 
 module.exports = (app) => {
 
-  app.get("/users", (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.json({
-      users: [{ name: "hcode", email: "contato@hcode.com.br", id: 1 }]
+  let route = app.route("/users");
+
+  route.get((req, res) => {
+    db.find({}).sort({ name: 1 }).exec((err, users) => {
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json({
+          users
+        });
+      }
     });
   });
 
-  app.post("/users", (req, res) => {
+  route.post((req, res) => {
+
+    if (!app.utils.validator.user(app, req, res)) return false;
+
     db.insert(req.body, (err, user) => {
       if (err) {
-        console.log(`error: ${err}`);
-        res.status(400).json({
-          error: err
-        });
+        app.utils.error.send(err, req, res);
       } else {
         res.status(200).json(user);
+      }
+    });
+  });
+
+  let routeId = app.route("/users/:id");
+
+  routeId.get((req, res) => {
+    db.findOne({ _id: req.params.id }).exec((err, user) => {
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(user);
+      }
+    });
+  });
+
+  routeId.put((req, res) => {
+
+    if (!app.utils.validator.user(app, req, res)) return false;
+
+    db.update({ _id: req.params.id }, req.body, err => {
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(Object.assign(req.params, req.body));
+      }
+    });
+  });
+
+  routeId.delete((req, res) => {
+    db.remove({ _id: req.params.id }, {}, err => {
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(req.params);
       }
     });
   });
